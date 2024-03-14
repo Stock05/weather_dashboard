b7fc993d6d5f21b73208775b2998e031520cb0b5
Stock05 <qace756@gmail.com>
2024-03-14 17:57:40 +0900

    데이터 프레임 표기

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 from utility import * 
 import streamlit as st
-
+import pandas as pd
 
 
 call_api(37.5,127.0)
@@ -13,6 +13,56 @@ print(data_kor)
 #trans_kor_time('data.json')
 st.header("날씨 정보 대시보드")
 st.info("위도와 경도를 입력해 날씨를 알수 있습니다")
+
+
+data_list=[]
+
+for count in range(len(data_kor['list'])):
+    temp = data_kor["list"][count]['main']['temp']
+    feels_like = data_kor["list"][count]['main']['feels_like']
+    temp_min = data_kor["list"][count]['main']['temp_min']
+    temp_max = data_kor["list"][count]['main']['temp_max']
+    humidity = data_kor["list"][count]['main']['humidity']
+    pressure = data_kor["list"][count]['main']['pressure']
+    clouds = data_kor["list"][count]['clouds']['all']    
+    deg = data_kor["list"][count]['wind']['deg']
+    speed = data_kor["list"][count]['wind']['speed']
+    weather = data_kor["list"][count]["weather"][0]["description"]
+    date = data_kor["list"][count]['kor_time']
+    data = {
+        '날짜': date,
+        '온도': temp,
+        '체감온도': feels_like,
+        '최저온도': temp_min,
+        '최고온도': temp_max,
+        '습도': humidity,
+        '기압': pressure,
+        '구름': clouds,
+        '풍향': deg,
+        '풍속': speed,
+        '날씨': weather
+        
+    }    
+    data_list.append(data)
+
+
+
+
+df = pd.DataFrame(data_list)
+pd.set_option('display.float_format', '{:.2f}'.format)
+st.dataframe(df, hide_index=True)
+
+
+
+
+
+
+    
+    
+
+
+
+
 
 
 
