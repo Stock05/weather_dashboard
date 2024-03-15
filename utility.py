ee40df4687d335b6efd35a1fd84636670c4273b4
Stock05 <qace756@gmail.com>
2024-03-15 13:45:43 +0900

    페이지 1 그래프

--- utility.py
+++ utility.py
@@ -4,6 +4,8 @@ import requests
 import json
 from dotenv import load_dotenv
 import os
+import pandas as pd
+import streamlit as st
 
 
 def call_api(lat,lon):
@@ -31,11 +33,33 @@ def read_json(filename):
     with open(filename, 'r') as file:                 
         return json.loads(file.read())
 
-
-def get_data():
-    temperature = data['list']['main']['temp']
-
-
-
-
-    
+def make_df(data_kor):
+    data_list=[]    
+    for count in range(len(data_kor['list'])):
+        temp = data_kor["list"][count]['main']['temp']
+        feels_like = data_kor["list"][count]['main']['feels_like']
+        temp_min = data_kor["list"][count]['main']['temp_min']
+        temp_max = data_kor["list"][count]['main']['temp_max']
+        humidity = data_kor["list"][count]['main']['humidity']
+        pressure = data_kor["list"][count]['main']['pressure']
+        clouds = data_kor["list"][count]['clouds']['all']    
+        deg = data_kor["list"][count]['wind']['deg']
+        speed = data_kor["list"][count]['wind']['speed']
+        weather = data_kor["list"][count]["weather"][0]["description"]
+        date = data_kor["list"][count]['kor_time']
+        data = {
+            '날짜': date,
+            '온도': temp,
+            '체감온도': feels_like,
+            '최저온도': temp_min,
+            '최고온도': temp_max,
+            '습도': humidity,
+            '기압': pressure,
+            '구름': clouds,
+            '풍향': deg,
+            '풍속': speed,
+            '날씨': weather
+            
+        }    
+        data_list.append(data)
+    return data_list    
